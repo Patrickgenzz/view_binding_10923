acbea42cfe905bf3adc8059cc26a34f5dd4d7bdb
Patrickgenzz <patrickgenzz@gmail.com>
2022-09-15 14:15:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10923/Task.kt
@@ -0,0 +1,5 @@
+package com.example.view_binding_10923
+
+data class Task (val chapter:String, val title:String, val details:String){
+
+}
---
+++ app/src/main/java/com/example/view_binding_10923/TaskList.kt
@@ -0,0 +1,13 @@
+package com.example.view_binding_10923
+
+object TaskList {
+    val tasklist = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur",
+            "Kebiasaan  sehari-hari untuk membuat hari makin indah"),
+        Task("Chapter 2", "Nguli genshin impact ", "Mencari Primo buat Gatcha mama Yaemiko tercinta / Keqing Wangi"),
+        Task("Chapter 3", "Ngopi", "Habis capek Nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "Dimana mama Ei?", "Buat aduk Mekanik showcase ayaka pyro"),
+        Task("Chapter 5", "Upload ke Tiktok",
+            "Buat Pamer ke view Showcase Ayaka Pyro C6 + Bennet C6"),
+    )
+}
